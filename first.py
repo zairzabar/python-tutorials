fc466bce8db8c3455f01904a896380701143ffbd
Ouzair Ansari <zuber.ansari.997@gmail.com>
2025-07-20 18:26:21 +0530

    First commit

---
+++ first.py
@@ -0,0 +1,5 @@
+price = 23.95
+shipping = 3.00
+
+print(price)
+print(shipping)
---
+++ third.py
@@ -0,0 +1,10 @@
+str = "The heart remembers what the mind forgets."
+
+print(str.replace("heart", "soul"))
+
+word = "mind"
+
+if word in str:
+    print("The word is in the string.")
+else:
+    print("The word is not in the string.") 
